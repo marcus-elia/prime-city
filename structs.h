ab200d8510363f4c3ae1041c10c8313f42066b3f
marcus-elia <mselia@uvm.edu>
2020-05-22 08:10:48 -0400

    Added structs file

---
+++ structs.h
@@ -0,0 +1,48 @@
+#ifndef PRIME_CITY_STRUCTS_H
+#define PRIME_CITY_STRUCTS_H
+
+// This file contains some structs and enums to make
+// including simpler
+
+
+// int points used for the chunks
+// in the xz plane
+struct Point2D
+{
+    int x;
+    int z;
+    bool operator ==(const Point2D& p1)
+    {
+        return x == p1.x && z == p1.z;
+    }
+    bool operator !=(const Point2D& p1)
+    {
+        return x != p1.x || z != p1.z;
+    }
+};
+
+
+struct Point
+{
+    double x;
+    double y;
+    double z;
+};
+
+// A line segment specified by two points
+struct Segment
+{
+    Point p1;
+    Point p2;
+};
+
+struct RGBAcolor
+{
+    double r;
+    double g;
+    double b;
+    double a;
+};
+
+
+#endif //PRIME_CITY_STRUCTS_H
