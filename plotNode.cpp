111165bab37398c3d7b4cbb1eae3fb3f8bcc09a0
marcus-elia <mselia@uvm.edu>
2020-06-27 09:15:09 -0400

    Filled in method for finding ID's of plots adjacent to a given plot ID

--- plotNode.cpp
+++ plotNode.cpp
@@ -110,5 +110,91 @@ int PlotNode::getID() const
 
 int makeID(Point2D chunkCoords, Point2D plotCoords, int plotsPerSide)
 {
-    return chunkCoords.toInt() + plotCoords.x + plotCoords.z * plotsPerSide;
+    return chunkCoords.toChunkID() + plotCoords.x + plotCoords.z * plotsPerSide;
+}
+int makeID(int chunkID, Point2D plotCoords, int plotsPerSide)
+{
+    return chunkID + plotCoords.x + plotCoords.z * plotsPerSide;
+}
+
+Point2D idToPlotCoords(int id, int plotsPerSide)
+{
+    int plotsPerChunk = plotsPerSide*plotsPerSide;
+    id = id % plotsPerChunk;
+    int x = id % plotsPerSide;
+    int z = id / plotsPerSide;
+    return {x, z};
+}
+
+int idAbove(int id, int plotsPerSide)
+{
+    int plotsPerChunk = plotsPerSide*plotsPerSide;
+    Point2D plotCoords = idToPlotCoords(id, plotsPerSide);
+    int chunkID = id / plotsPerChunk;
+    int x = plotCoords.x;
+    int z = plotCoords.z;
+    if(z > 0) // If it's in the same chunk
+    {
+        return makeID(chunkID, {x, z-1}, plotsPerSide);
+    }
+    else     // If it's in the chunk above this one
+    {
+        Point2D chunkCoords = chunkIDtoPoint(chunkID);
+        chunkCoords.z -= 1; // move up one chunk
+        return makeID(chunkCoords, {x, plotsPerSide-1}, plotsPerSide);
+    }
+}
+int idBelow(int id, int plotsPerSide)
+{
+    int plotsPerChunk = plotsPerSide*plotsPerSide;
+    Point2D plotCoords = idToPlotCoords(id, plotsPerSide);
+    int chunkID = id / plotsPerChunk;
+    int x = plotCoords.x;
+    int z = plotCoords.z;
+    if(z < plotsPerSide - 1) // If it's in the same chunk
+    {
+        return makeID(chunkID, {x, z+1}, plotsPerSide);
+    }
+    else     // If it's in the chunk above this one
+    {
+        Point2D chunkCoords = chunkIDtoPoint(chunkID);
+        chunkCoords.z += 1; // move down one chunk
+        return makeID(chunkCoords, {x, 0}, plotsPerSide);
+    }
+}
+int idLeft(int id, int plotsPerSide)
+{
+    int plotsPerChunk = plotsPerSide*plotsPerSide;
+    Point2D plotCoords = idToPlotCoords(id, plotsPerSide);
+    int chunkID = id / plotsPerChunk;
+    int x = plotCoords.x;
+    int z = plotCoords.z;
+    if(x > 0) // If it's in the same chunk
+    {
+        return makeID(chunkID, {x-1, z}, plotsPerSide);
+    }
+    else     // If it's in the chunk above this one
+    {
+        Point2D chunkCoords = chunkIDtoPoint(chunkID);
+        chunkCoords.x -= 1; // move left one chunk
+        return makeID(chunkCoords, {plotsPerSide-1, z}, plotsPerSide);
+    }
+}
+int idRight(int id, int plotsPerSide)
+{
+    int plotsPerChunk = plotsPerSide*plotsPerSide;
+    Point2D plotCoords = idToPlotCoords(id, plotsPerSide);
+    int chunkID = id / plotsPerChunk;
+    int x = plotCoords.x;
+    int z = plotCoords.z;
+    if(x < plotsPerSide-1) // If it's in the same chunk
+    {
+        return makeID(chunkID, {x+1, z}, plotsPerSide);
+    }
+    else     // If it's in the chunk above this one
+    {
+        Point2D chunkCoords = chunkIDtoPoint(chunkID);
+        chunkCoords.x += 1; // move right one chunk
+        return makeID(chunkCoords, {0, z}, plotsPerSide);
+    }
 }
