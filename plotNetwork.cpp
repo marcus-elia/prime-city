f91a96c3695d36a85fe1eb4d5bd28242cf6e69a9
marcus-elia <mselia@uvm.edu>
2020-06-28 12:07:36 -0400

    Added breadth first search function

--- plotNetwork.cpp
+++ plotNetwork.cpp
@@ -46,11 +46,109 @@ void PlotNetwork::addNode(PlotNode n)
 // reverse order
 std::vector<PlotNode*> PlotNetwork::breadthFirstSearch(int idStart, int idEnd) const
 {
-    return std::vector<PlotNode*>();
+    std::vector<PlotNode*> output;
+
+    // Keep track of two layers of nodes at each step by storing
+    // their plotID's
+    std::vector<int> prevLevel;
+    std::vector<int> curLevel;
+
+    // Map each node visited to the previous node that was used
+    // to get to it
+    std::unordered_map<int, int> node2prev;
+
+    curLevel.push_back(idStart);
+    bool hasBeenFound = false;
+
+    while(!hasBeenFound)
+    {
+        prevLevel = curLevel;
+        curLevel = std::vector<int>();
+        // Iterate through the previous level and look at each neighbor
+        for(int plotID : prevLevel)
+        {
+            // Check the neighbor above this plot
+            if(id2node.at(plotID)->hasUpNeighbor())
+            {
+                int upNeighborID = idAbove(plotID, plotsPerSide);
+                if(node2prev.count(upNeighborID) == 0)
+                {
+                     node2prev[upNeighborID] = plotID;
+                     curLevel.push_back(upNeighborID);
+                     if(upNeighborID == idEnd)
+                     {
+                         hasBeenFound = true;
+                         break;
+                     }
+                }
+            }
+            // Check the neighbor below this plot
+            if(id2node.at(plotID)->hasDownNeighbor())
+            {
+                int downNeighborID = idBelow(plotID, plotsPerSide);
+                if(node2prev.count(downNeighborID) == 0)
+                {
+                    node2prev[downNeighborID] = plotID;
+                    curLevel.push_back(downNeighborID);
+                    if(downNeighborID == idEnd)
+                    {
+                        hasBeenFound = true;
+                        break;
+                    }
+                }
+            }
+            // Check the neighbor left of this plot
+            if(id2node.at(plotID)->hasLeftNeighbor())
+            {
+                int leftNeighborID = idLeft(plotID, plotsPerSide);
+                if(node2prev.count(leftNeighborID) == 0)
+                {
+                    node2prev[leftNeighborID] = plotID;
+                    curLevel.push_back(leftNeighborID);
+                    if(leftNeighborID == idEnd)
+                    {
+                        hasBeenFound = true;
+                        break;
+                    }
+                }
+            }
+            // Check the neighbor right of this plot
+            if(id2node.at(plotID)->hasRightNeighbor())
+            {
+                int rightNeighborID = idRight(plotID, plotsPerSide);
+                if(node2prev.count(rightNeighborID) == 0)
+                {
+                    node2prev[rightNeighborID] = plotID;
+                    curLevel.push_back(rightNeighborID);
+                    if(rightNeighborID == idEnd)
+                    {
+                        hasBeenFound = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    // Trace backwards and add the path to the output
+    int curID = idEnd;
+    while(curID != idStart)
+    {
+        output.push_back(id2node.at(curID));
+        curID = node2prev[curID];
+    }
+    output.push_back(id2node.at(idStart));
+    return output;
 }
 
 // Wrapper function
 std::vector<PlotNode*> PlotNetwork::getShortestPath(int idStart, int idEnd) const
 {
-    return std::vector<PlotNode*>();
+    std::vector<PlotNode*> output;
+    std::vector<PlotNode*> reverseNodes = breadthFirstSearch(idStart, idEnd);
+    for(int i = reverseNodes.size() - 1; i >= 0; i--)
+    {
+        output.push_back(reverseNodes[i]);
+    }
+    return output;
 }
