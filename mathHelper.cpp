0a9913dafcb72bfdd6629ed32cde5b4a71b6f0c1
marcus-elia <mselia@uvm.edu>
2020-07-16 21:27:42 -0400

    2,3,5,7 are now prime

--- mathHelper.cpp
+++ mathHelper.cpp
@@ -32,6 +32,10 @@ bool twoDigitIsPrime(int n)
     {
         return false;
     }
+    if(n == 2 || n == 3 || n == 5 || n == 7)
+    {
+        return true;
+    }
     return n % 2 != 0 && n % 3 != 0 && n % 5 != 0 && n % 7 != 0;
 }
 
